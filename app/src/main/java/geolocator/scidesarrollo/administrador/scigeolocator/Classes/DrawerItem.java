c85a037984891fc529750cdd85098e915bac61e2
Fabio Posada <fabio.posada@softwaresci.com>
2015-06-24 12:24:36 -0500

    Initial commit

---
+++ app/src/main/java/geolocator/scidesarrollo/administrador/scigeolocator/Classes/DrawerItem.java
@@ -0,0 +1,29 @@
+package geolocator.scidesarrollo.administrador.scigeolocator.Classes;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Administrador on 23/06/2015.
+ */
+public class DrawerItem {
+
+    Drawable itemIcon;
+    String itemTitle;
+
+    public DrawerItem(Drawable itemIcon, String itemTitle) {
+
+        this.itemIcon = itemIcon;
+        this.itemTitle = itemTitle;
+
+    }
+
+    public Drawable getItemIcon() {
+        return itemIcon;
+    }
+
+    public String getItemTitle() {
+        return itemTitle;
+    }
+}
+
+
